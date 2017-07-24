818f11bcde85c1a0067cc361e705b861992303de
Chua Wei Kuan <khankuan@gmail.com>
2017-07-24 14:21:23 -0700

    Updates

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider } from 'react-apollo';
+import { networkInterface } from './graphql/networkInterface';
+import App from './App';
+
+const client = new ApolloClient({ networkInterface });
+
+ReactDOM.render(
+  <ApolloProvider client={client}><App /></ApolloProvider>,
+  document.getElementById('root'),
+);
