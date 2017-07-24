4b9cda14d80dcb2b9673bcdf29fa3268ceac2822
Chua Wei Kuan <khankuan@gmail.com>
2017-07-24 14:31:30 -0700

    update deploy

--- src/App.js
+++ src/App.js
@@ -10,18 +10,8 @@ class App extends Component {
         <header>
           <h1>Apollo Client Error Template</h1>
           <p>
-            This is a template that you can use to demonstrate an error in
-            Apollo Client. Edit the source code and watch your browser window
-            reload with the changes.
-          </p>
-          <p>
-            The code which renders this component lives in{" "}
-            <code>./src/App.js</code>.
-          </p>
-          <p>
-            The GraphQL schema is in <code>./src/graphql/schema</code>.
-            Currently the schema just serves a list of people with names and
-            ids.
+            To reproduce: Click on the first fetch more button. Notice that the
+            second list gets updated as well.
           </p>
         </header>
         <ComponentOne />
